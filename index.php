508c1fa0468625a2bc9a32f7b1e912ccbd91adbe
Konrad Iwanczewski <konrad.iwanczewski@student.pk.edu.pl>
2025-10-13 08:30:19 +0200

    js, html, css

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
 <?php
 
-echo "<h1> Hi Students! 😎</h1>";
+#echo "<h1> Hi Students! 😎</h1>";
+
+require_once 'Routing.php';
+
+$path = trim($_SERVER["REQUEST_URI"],"/");
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::run($path);
+
+# echo $path;
+
+# var_dump($path);
+
+
+#kody http
+#100
+#200 
+#300
+#400 np. 404
+#500
+#
+
+echo "
+<script>
+    const header = document.querySelector('h1');
+    console.log(header);
+    header.addEventListener('click', () => {
+        header.style.color = 'green';
+    })
+</script>
+";
+
