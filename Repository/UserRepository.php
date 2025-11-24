b9f29cb14641f9279d1ec6643a32423950441b6f
Konrad I. <konrad.iwan@op.pl>
2025-11-24 08:54:31 +0100

    database

---
+++ Repository/UserRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'Repository.php';
+
+class UserRepository extends Repository {
+    public function getUsers(): ?array {
+        $query = $this->database->connect()->prepare('
+        SELECT * FROM user
+        ');
+        $query->execute();
+
+        $users = $query->fetchAll(PDO::FETCH_ASSOC);
+        return $users;
+    }
+}
+
