972abe85ae782892e957a402d6d952feb31e3f3d
Konrad I. <konrad.iwan@op.pl>
2026-01-09 15:34:30 +0100

    zupełna zmiana struktury plików i katalogów

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Plik jednak używany!!! Zmieniony został na .env, żeby nie było danych logowania na sztywno w pliku, tylko w zmiennych środowiskowych
-// Na razie są dane na sztywno, później dopiero to trzeba zmienić, jeszcze nie
-
-const USERNAME = 'docker';
-const PASSWORD = 'docker';
-const HOST = 'db';
-const DATABASE = 'db';
